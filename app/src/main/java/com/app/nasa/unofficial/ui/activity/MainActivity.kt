1e33fa1795a082f96d201d2576c9f28d6efef340
Anant00 <anantpundir02@gmail.com>
2019-12-12 18:00:49 +0530

    hook navigation to bottombar

--- app/src/main/java/com/app/nasa/unofficial/ui/activity/MainActivity.kt
+++ app/src/main/java/com/app/nasa/unofficial/ui/activity/MainActivity.kt
@@ -2,12 +2,23 @@ package com.app.nasa.unofficial.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.app.nasa.unofficial.R
+import com.app.nasa.unofficial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            val navController =
+                Navigation.findNavController(this@MainActivity, R.id.mainNavigationFragment)
+            bottomNavigation.setupWithNavController(navController)
+        }
     }
 }
