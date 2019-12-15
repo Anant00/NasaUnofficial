e5b30751efb6a035b27e28b1dd3720af90b70c24
Anant00 <anantpundir02@gmail.com>
2019-12-15 15:03:55 +0530

    fix navigation

--- app/src/main/java/com/app/nasa/unofficial/di/FragmentBuildersModule.kt
+++ app/src/main/java/com/app/nasa/unofficial/di/FragmentBuildersModule.kt
@@ -1,5 +1,6 @@
 package com.app.nasa.unofficial.di
 
+import com.app.nasa.unofficial.ui.fragments.MarsFragment
 import com.app.nasa.unofficial.ui.fragments.PictureOfTheDayFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -9,4 +10,7 @@ abstract class FragmentBuildersModule {
 
     @ContributesAndroidInjector
     abstract fun contributePODFragment(): PictureOfTheDayFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeMarsFragment(): MarsFragment
 }
--- app/src/main/java/com/app/nasa/unofficial/ui/activity/MainActivity.kt
+++ app/src/main/java/com/app/nasa/unofficial/ui/activity/MainActivity.kt
@@ -3,7 +3,7 @@ package com.app.nasa.unofficial.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.app.nasa.unofficial.R
 import com.app.nasa.unofficial.databinding.ActivityMainBinding
@@ -15,10 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.apply {
-            val navController =
-                Navigation.findNavController(this@MainActivity, R.id.mainNavigationFragment)
-            bottomNavigation.setupWithNavController(navController)
-        }
+        val navController = findNavController(R.id.mainNavigationFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
